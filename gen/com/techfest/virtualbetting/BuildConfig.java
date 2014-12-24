97c564b52f8b353349f989038f7ca8e09fd7f149
nithin murali <imnmfotmal@gmail.com>
2014-12-24 16:19:31 +0530

    initial commit

---
+++ gen/com/techfest/virtualbetting/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.techfest.virtualbetting;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/techfest/virtualbetting/SplashScreen.java
@@ -0,0 +1,5 @@
+package com.techfest.virtualbetting;
+
+public class SplashScreen {
+
+}
