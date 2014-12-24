9baef7c6e9b4eb59ad9ab6dfb267880e3c732f34
nithin murali <imnmfotmal@gmail.com>
2014-12-25 03:14:15 +0530

    basic main ui

--- src/com/techfest/virtualbetting/MainActivity.java
+++ src/com/techfest/virtualbetting/MainActivity.java
@@ -4,13 +4,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-
+	
+	TextView uid;
+	TextView balence,lanem,rname;
+	EditText betamount;
+	Button bet;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 	
 	@Override
