bb43484f724e567fe74ac2e80b8ccd9a8771238d
nithin murali <imnmfotmal@gmail.com>
2014-12-25 02:00:45 +0530

    register user done

--- gen/com/techfest/virtualbetting/R.java
+++ gen/com/techfest/virtualbetting/R.java
@@ -1632,7 +1632,11 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020030;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
-        public static final int ic_launcher=0x7f020033;
+        public static final int fail=0x7f020033;
+        public static final int gradient_background=0x7f020034;
+        public static final int ic_launcher=0x7f020035;
+        public static final int success=0x7f020036;
+        public static final int virtual_bg=0x7f020037;
     }
     public static final class id {
         public static final int action_bar=0x7f090031;
@@ -1648,7 +1652,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f09003f;
+        public static final int action_settings=0x7f090041;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
@@ -1694,6 +1698,8 @@ containing a value of this type.
         public static final int split_action_bar=0x7f090004;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
+        public static final int textView1=0x7f09003f;
+        public static final int textView2=0x7f090040;
         public static final int title=0x7f090027;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
@@ -1750,7 +1756,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_splash=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- src/com/techfest/virtualbetting/SplashScreen.java
+++ src/com/techfest/virtualbetting/SplashScreen.java
@@ -1,5 +1,158 @@
 package com.techfest.virtualbetting;
 
-public class SplashScreen {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.techfest.library.JsonParser;
+import com.techfest.library.ConnectionDetector;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashScreen extends Activity {
+	
+	public static final String USER_DATA = "usr_data";
+	public static final String registerCode = "1234qwert"; // change it
+	public static final String UserId_pref = "uid";
+	public static final String balence_pref = "amount";
+	private static String baseURL = "http://testapi.com/";
+	
+	ConnectionDetector cd;
+	Boolean netPresent,error;
+	final Context context = this;
+	String uid;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		/*
+		 * Showing splashscreen while making network calls to download necessary
+		 * data before launching the app Will use AsyncTask to make http call
+		 */
+		cd = new ConnectionDetector(getApplicationContext());
+		netPresent = cd.isConnectingToInternet();
+		
+		if (netPresent == false ) {
+			//maybe try connecitng a few more times
+			
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+	 
+				// set title
+				alertDialogBuilder.setTitle("Internet Connection");
+	 
+				// set dialog message
+				alertDialogBuilder
+					.setMessage("Can't conenct to Internet")
+					.setCancelable(false)
+					.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog,int id) {
+							// if this button is clicked, close
+							// current activity
+							SplashScreen.this.finish();
+						}
+					  });
+	 
+					// create alert dialog
+					AlertDialog alertDialog = alertDialogBuilder.create();
+	 
+					// show it
+					alertDialog.show();
+
+		} else {
+
+			new PrefetchData().execute();
+	
+		}
+		
+	}
+
+	
+	/*
+	 * Async Task to make http call
+	 */
+	private class PrefetchData extends AsyncTask<Void, Void, Void> {
+
+		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			// before making http calls
+			Log.e("JSON", "Pre execute");
+
+		}
+
+		@Override
+		protected Void doInBackground(Void... arg0) {
+			
+			JsonParser jsonParser = new JsonParser();
+			SharedPreferences prefs = getSharedPreferences(USER_DATA, MODE_PRIVATE);
+			SharedPreferences.Editor editor = getSharedPreferences(USER_DATA, MODE_PRIVATE).edit();
+			
+			uid = prefs.getString("text", null);
+			
+			if (uid == null) {
+				//register user
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("code", registerCode));
+				JSONObject json = jsonParser.getJSONFromUrl(baseURL + "admin/registerUsr", params);
+				Log.e("Response: ", "> " + json);
+				if (json != null) {
+					try {
+						//fetch userid
+						JSONObject json_user = json.getJSONObject("user");
+						uid = json_user.getString("uid");
+						//save uid
+						editor.putString("uid", uid);
+						editor.putInt("balence", 1000);
+						editor.commit();
+						Log.e("JSON", "> " +uid + "registred");
+
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+				
+			} else {
+				//user already registred
+				//cross check uid
+				
+			}
+			
+			
+			
+		return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			// After completing http call
+			// will close this activity and lauch main activity
+			Intent i = new Intent(SplashScreen.this, MainActivity.class);
+			i.putExtra("uid", uid);
+			startActivity(i);
+
+			// close this activity
+			finish();
+		}
+
+	}
+	
 }
